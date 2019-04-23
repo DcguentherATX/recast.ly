011d17d30d062f466a128612ed4ef243738547fc
DcguentherATX <davidcarlguenther@gmail.com>
2019-04-22 22:56:31 -0500

    got generic layout to work but got hung up on mapping videolist entries to videolist, after consultation should be able to get it done in the morning

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,39 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import exampleVideoData from '../data/exampleVideoData.js';
+import videoListEntry from './VideoListEntry.js';
 
+console.log(exampleVideoData);
+
+var VideoList = (props) => {
+  const videos = props.videos;
+  let listItems = videos.map((video) =>
+    <ListItem key={video.toString()}
+      value={video} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+};
+
+
+
+// class VideoList extends React.Component(){
+//   constructor(props){
+//     super(props);
+//   }
+//   render(){
+//   <div className="video-list">
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   </div>
+//   }
+// }
+
+// ReactDOM.render(<>);
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
